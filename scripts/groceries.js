32bf74086e0ac371e2761c74b96a2c9e9eb05b02
IleriOluwa Fasubaa <ilerioluwafasubaa@IleriOluwas-MacBook-Pro.local>
2024-01-26 12:27:57 -0500

    organic + css

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -7,74 +7,107 @@ var products = [
 		name: "brocoli",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 1.99
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 2.35
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
 		price: 10.00
 	},
 	{
 		name: "pasta",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 3.00
 	},
 	{
 		name: "rice",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 2.49
 	},
 	{
 		name: "ground-beef",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
 		price: 14.99
 	},
 	{
 		name: "onion",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 1.00
 	},
 	{
 		name: "apple",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 0.50
 	},
 	{
 		name: "cereal",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 6.00
 	},
 	{
 		name: "milk",
 		vegetarian: true,
 		glutenFree: true,
+		organic: false,
 		price: 3.50
 	},
 	{
 		name: "chicken",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
 		price: 11.99
 	},
 	{
 		name: "mushroom",
 		vegetarian: true,
 		glutenFree: true,
+		organic: true,
 		price: 3.75
-	}
+	},
+	{
+		name: "Spinach",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 11.99
+	},
+	{
+		name: "kale",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 11.99
+	},
+	{
+		name: "grapes",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 11.99
+	},
 ];
 	
 
@@ -91,6 +124,9 @@ function restrictListProducts(prods, restriction) {
 			product_names.push(prods[i].name);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Organic") && (prods[i].organic == true)){
 			product_names.push(prods[i].name);
 		}
 		else if (restriction == "None"){
