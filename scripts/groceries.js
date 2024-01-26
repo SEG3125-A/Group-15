0554fa145368069bd12474191fead8ac3a4be225
Keynaan I <key25652@gmail.com>
2024-01-26 13:11:41 -0500

    Added mixed options + fixed prices

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -121,16 +121,31 @@ function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i].name+" $"+ prods[i].price);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i].name+" $"+ prods[i].price);
 		}
 		else if ((restriction == "Organic") && (prods[i].organic == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i].name+" $"+ prods[i].price);
 		}
+		else if ((restriction == "VegetarianAndGlutenFree") && (prods[i].glutenFree == true) && (prods[i].vegetarian == true) ){
+            product_names.push(prods[i].name+" $"+ prods[i].price);
+        }
+		else if ((restriction == "VegetarianAndOrganic") && (prods[i].organic == true) && (prods[i].vegetarian == true) ){
+            product_names.push(prods[i].name+" $"+ prods[i].price);
+        }
+
+		else if ((restriction == "GlutenFreeAndOrganic") && (prods[i].organic == true) && (prods[i].glutenFree == true) ){
+            product_names.push(prods[i].name+" $"+ prods[i].price);
+        }
+
+		else if ((restriction == "AllRestrictions") && (prods[i].organic == true) && (prods[i].glutenFree == true) && prods[i].vegetarian == true ){
+            product_names.push(prods[i].name+" $"+ prods[i].price);
+        }
+
 		else if (restriction == "None"){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i].name+" $"+ prods[i].price);
 		}
 	}
 	return product_names;
@@ -140,11 +155,13 @@ function restrictListProducts(prods, restriction) {
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
-	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
-			totalPrice += products[i].price;
-		}
-	}
-	return totalPrice;
+    totalPrice = 0;
+    for (let i=0; i<products.length; i+=1) {
+        if (chosenProducts.indexOf(products[i].name+" $"+ products[i].price) > -1){
+            totalPrice += products[i].price;
+        }
+    }
+    return totalPrice;
 }
+
+
