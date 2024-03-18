abb97a3a5407a5158079657e5850b5937b48d388
Keynaan I <key25652@gmail.com>
2024-03-17 23:34:26 -0400

    Styling changes

--- Lab8/lab8r/src/components/ReviewCard.js
+++ Lab8/lab8r/src/components/ReviewCard.js
@@ -27,20 +27,20 @@ export default function ReviewCard({addReview}) {
           
         <div style={{margin: 10}}>
             Name: 
-            <input style={{borderRadius: 5, height: 20, marginLeft: 15}} type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <input style={{borderRadius: 5, height: 20, marginLeft: 15, fontSize: 18}} type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
         <div style={{margin: 10}}>
             Media 
-            <input style={{borderRadius: 5, height: 20, marginLeft: 15}} type="text" name="name" value={media} onChange={(e) => setMedia(e.target.value)}/> 
+            <input style={{borderRadius: 5, height: 20, marginLeft: 15, fontSize: 18}} type="text" name="name" value={media} onChange={(e) => setMedia(e.target.value)}/> 
         </div>
         <div style={{margin: 10}}>
             Date 
-            <input style={{borderRadius: 5, height: 20, marginLeft: 15}} type="text" name="name" value={date} onChange={(e) => setDate(e.target.value)} />
+            <input style={{borderRadius: 5, height: 20, marginLeft: 15, fontSize: 18}} type="text" name="name" value={date} onChange={(e) => setDate(e.target.value)} />
         </div>
         
         </div>
         <h4>Review:</h4>
-        <input style={{height: 60, width: '80%', marginLeft: 50, borderRadius: 10, marginBottom: 10}} type="text" name="review" value={review} onChange={(e) => setReview(e.target.value)}/>
+        <input style={{height: 60, width: '80%', marginLeft: 50, borderRadius: 10, marginBottom: 10, fontSize: 18}} type="text" name="review" value={review} onChange={(e) => setReview(e.target.value)}/>
         
         <button style={{height: 40, width: '80%', marginLeft: 60, backgroundColor: 'red', color: 'white', borderRadius: 10, fontSize: 15, fontWeight: 'bold'}} type='submit'>Submit</button>
         </form>
