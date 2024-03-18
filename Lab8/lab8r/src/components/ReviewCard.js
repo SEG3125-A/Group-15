64b6af51bd5ec7c44d153a6ac64c3b23541274fc
IleriOluwa Fasubaa <ilerioluwafasubaa@IleriOluwas-MacBook-Pro.local>
2024-03-17 20:44:18 -0400

    postreview

--- Lab8/lab8r/src/components/ReviewCard.js
+++ Lab8/lab8r/src/components/ReviewCard.js
@@ -2,25 +2,26 @@ import React from 'react'
 
 export default function ReviewCard() {
   return (
-    <div style={{height: 'auto', width: '80%', backgroundColor: 'grey', borderRadius: 5, padding: 10, marginTop: 20}}>
+    <div style={{height: 150, width: '80%', backgroundColor: 'lightgrey', borderRadius: 5, padding: 20, marginTop: 20, borderWidth: 2, borderColor: 'black'}}>
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
         <div style={{margin: 10}}>
             Name: 
-            <input style={{borderRadius: 5}} type="text" name="name" />
+            <input style={{borderRadius: 5, height: 20}} type="text" name="name" />
         </div>
         <div style={{margin: 10}}>
             Score 
-            <input style={{borderRadius: 5}} type="text" name="name" />
+            <input style={{borderRadius: 5, height: 20}} type="text" name="name" />
         </div>
         <div style={{margin: 10}}>
             Date 
-            <input style={{borderRadius: 5}} type="text" name="name" />
+            <input style={{borderRadius: 5, height: 20}} type="text" name="name" />
         </div>
         
         </div>
         Reiew:
-        <input style={{height: 40, width: '80%', marginLeft: 10, borderRadius: 10}} type="text" name="review" />
+        <input style={{height: 40, width: '80%', marginLeft: 10, borderRadius: 10, marginBottom: 10}} type="text" name="review" />
         
+        <button style={{height: 40, width: '80%', marginLeft: 60, backgroundColor: 'red', color: 'white', borderRadius: 10}}>Submit</button>
     </div>
   )
 }
