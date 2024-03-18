5a97c08468e9ee9e0e6c3a84071e7766916f0140
IleriOluwa Fasubaa <ilerioluwafasubaa@IleriOluwas-MacBook-Pro.local>
2024-03-17 22:38:55 -0400

    functionality

--- Lab8/lab8r/src/App.js
+++ Lab8/lab8r/src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import ReviewCard from './components/ReviewCard';
 import Review from './components/Review';
+//import { reviews } from './reviews';
+
+
 
 function App() {
+ const [reviews, setReviews] = useState([
+    ['John', 'Mad Max','2024-01-01','Very good movie'],
+    ['Mary', 'Home Alone','2024-01-01','great movie'],
+    ['Benson', 'Fast and Furious','2024-01-01','not so great'],
+    ['Diana', 'Gran Turismo','2024-01-01','nice movie...']
+  ]);
+
+  const addReview = (names, media,date, review) => {
+    setReviews([...reviews, [names, media, date, review]]);
+  }
   return (
     <div >
       <Header/>
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
         <div>
-        <ReviewCard/>
+        <ReviewCard addReview={addReview}/>
         </div>
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', marginTop: 120}}>
-          <Review/>
-          <Review/>
-          <Review/>
-          <Review/>
+        {reviews.map((review, index) => (
+      <Review
+        //key={index}
+        name={review[0]}
+        media={review[1]}
+        date={review[2]}
+        review={review[3]}
+      />
+    ))}
         </div>
       
       </div>
--- Lab8/lab8r/src/components/Review.js
+++ Lab8/lab8r/src/components/Review.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-export default function Review() {
+export default function Review({name, media, date, review}) {
+    
   return (
     <div style={{height: 220, width: 900, backgroundColor: 'lightgrey', borderRadius: 5, padding: 20, margin: 20, borderWidth: 2, borderColor: 'black'}}>
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
     <div style={{margin: 10}}>
          
-        <h4>Name: Name Placeholder</h4> 
+        <h4>Name: {name}</h4> 
     </div>
     <div style={{margin: 10}}>
-    <h4>Score: Score Placeholder</h4> 
+    <h4>Media: {media}</h4> 
     </div>
     <div style={{margin: 10}}>
-    <h4>Date: Date Placeholder</h4> 
+    <h4>Date: {date}</h4> 
     </div>
     
     </div>
-    <h4 style={{marginBottom: 50}}>Review: Review Placeholder</h4> 
+    <h4 style={{marginBottom: 50}}>Review: {review}</h4> 
     
     <button style={{height: 40, width: 60, marginLeft: 20, backgroundColor: 'red', color: 'white', borderRadius: 10, fontSize: 15, fontWeight: 'bold'}}>Like</button>
     <button style={{height: 40, width: 60, marginLeft: 60, backgroundColor: 'red', color: 'white', borderRadius: 10, fontSize: 15, fontWeight: 'bold'}}>Dislike</button>
